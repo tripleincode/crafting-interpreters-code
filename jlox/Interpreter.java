4c92c8244335871cf5115352fbf983485292f48f
triple <triple943545@gmail.com>
2023-07-15 14:27:23 -0700

    forgot to save interpreter.java

--- jlox/Interpreter.java
+++ jlox/Interpreter.java
@@ -51,9 +51,6 @@ class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof Double && right instanceof Double) {
                     return (double)left + (double)right;
                 }
-                if (left instanceof String && right instanceof String) {
-                    return (String)left + (String)right;
-                }
                 throw new RuntimeError(expr.operator,
                     "Operands must be compatible types.");
             case SLASH:
